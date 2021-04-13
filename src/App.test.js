a9895d82651e067ebeda5eefab2e735672843c75
Mahesh <mw60680@gmail.com>
2021-04-14 00:42:07 +0530

    Updated gitignore for vscode settings dir. Basic test case updated.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+afterEach(cleanup);
+
+test('renders learn react link', () => {
+  const { asFragment } = render(<App />);
+
+  expect(asFragment(<App />)).toMatchSnapshot();
+});
